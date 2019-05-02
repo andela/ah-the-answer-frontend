e30401bce364c385e4e9b5d9dbb0d47dafad1217
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-02 21:47:43 +0300

    [164829313] Removes sample code

--- src/store/actions/articleActions.js
+++
@@ -1,8 +0,0 @@
-const createArticle = (article) => {
-  return (dispatch, getState) => {
-    // make async call to database
-    dispatch({ type: 'CREATE_ARTICLE', article });
-  };
-};
-
-export default createArticle;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import ArticleList from '../components/articles/ArticleList';
 
 
-function Home(props) {
-  const { articles } = props;
-
+function Home() {
   return (
     <div>
       <h4 className="center">Home</h4>
-      <ArticleList articles={articles} />
     </div>
   );
 }
 
-Home.propTypes = {
-  articles: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    body: PropTypes.string.isRequired,
-  })),
-};
-Home.defaultProps = {
-  articles: {
-    description: '',
-  },
-};
-
-const mapStateToProps = state => ({ articles: state.articles.articles });
-
-export default connect(mapStateToProps)(Home);
+export default connect()(Home);
