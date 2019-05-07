0712050bd6fb7d479c92851acbf9c897b05cb6ca
Kyppy Simani <kyppy.simani@andela.com>
2019-05-07 22:27:15 +0300

    Made 'badgeButton' a class-based component

--- src/components/ButtonBadge.js
+++ src/components/ButtonBadge.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const ButtonBadge = (props) => {
-  return (
-    <div className="container">
-      <button type="button" onClick = {props.updateFollows} className="btn btn-primary">
-          {props.buttonName}
-        <span className="badge">
-           {props.badgeNumber}
-        </span>
-      </button>
-    </div>
-  );
-};
+class ButtonBadge extends Component {
+  componentDidMount() {
+    this.props.badgeUpdateMethod();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <button type="button" className="btn btn-primary">
+          {this.props.buttonName}
+          <span className="badge">
+            {this.props.badgeNumber}
+          </span>
+        </button>
+      </div>
+    );
+  }
+}
 
 
 export default ButtonBadge;
