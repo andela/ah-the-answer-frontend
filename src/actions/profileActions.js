7e2b1d8efaae33d3a6d47a64eee3033a53f55c7b
Kyppy Simani <kyppy.simani@andela.com>
2019-05-08 00:40:04 +0300

    Created 'follower' and 'user bio' update methods

--- src/actions/profileActions.js
+++ src/actions/profileActions.js
@@ -18,6 +18,30 @@ export const updateFollows = () => {
   };
 };
 
-export const updateFollowers = () => ({
-  type: ProfileAction.UPDATE_FOLLOWERS,
-});
+export const updateFollowers = () => {
+  return (dispatch) => {
+    axios.get(' http://127.0.0.1:8000/api/follows/count/Kyppy/')
+      .then((response) => {
+        // handle success
+        dispatch({ type: ProfileAction.UPDATE_FOLLOWERS, followerCount: response.data.success[1].followers });
+      })
+      .catch((error) => {
+        // handle error
+        console.log('Error fecthing and parsing data', error);
+      });
+  };
+};
+
+export const updateBio = () => {
+  return (dispatch) => {
+    axios.get(' http://127.0.0.1:8000/api/profiles/Kyppy/')
+      .then((response) => {
+        // handle success
+        dispatch({ type: ProfileAction.UPDATE_BIO, followerCount: response.data.profile.user_bio });
+      })
+      .catch((error) => {
+        // handle error
+        console.log('Error fecthing and parsing data', error);
+      });
+  };
+};
