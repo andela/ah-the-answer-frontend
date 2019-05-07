d21935c27e0805d7a812a683ca76c2b4b3a60904
Kyppy Simani <kyppy.simani@andela.com>
2019-05-08 00:39:25 +0300

    Update import route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './themes/custom.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './views/Navbar'
-import ProfileView from './containers/profile/profile';
+import ProfileView from './containers/profile/profileContainer';
 
 function App() {
   return (
