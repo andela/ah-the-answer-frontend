cd449c066b6fc78c328700cc2cd7df3271336096
Kyppy Simani <kyppy.simani@andela.com>
2019-05-07 21:37:13 +0300

    Include profileReducer in rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import articleReducer from './articleReducer';
+import profileReducer from './profileReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   articles: articleReducer,
+  profile: profileReducer,
 });
 
 export default rootReducer;
