f81d0cadf89347873b42508a5b70ca1812e0ed48
Kyppy Simani <kyppy.simani@andela.com>
2019-05-08 11:25:45 +0300

    Corrected string name

--- src/actiontypes/profiles/profileActionTypes.js
+++ src/actiontypes/profiles/profileActionTypes.js
@@ -1,4 +1,4 @@
 export const UPDATE_FOLLOWS = 'profile/UPDATE_FOLLOWS';
 export const UPDATE_FOLLOWERS = 'profile/UPDATE_FOLLOWERS';
 export const UPDATE_BIO = 'profile/UPDATE_BIO';
-export const UPDATE_NAME = 'profile/UPDATE_BIO';
+export const UPDATE_NAME = 'profile/UPDATE_NAME';
