e885f345c004225b3f6cdc14f451c2dc83451d42
Kyppy Simani <kyppy.simani@andela.com>
2019-05-09 23:32:27 +0300

    Updated container import route

--- src/containers/profile/profileView.js
+++ src/containers/profile/profileView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import profileContainer from './profileContainer';
-import ProfileUpdate from '../../components/ProfileUpdate';
+import ProfileUpdate from './ProfileUpdate';
 
 const ViewProfile = () => (
   <BrowserRouter>
