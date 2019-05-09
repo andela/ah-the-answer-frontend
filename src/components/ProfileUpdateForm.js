697d46b78a1e03b9c60259acc54692500f45b210
Kyppy Simani <kyppy.simani@andela.com>
2019-05-09 17:36:21 +0300

    Implemented profile update functionality

--- src/components/ProfileUpdateForm.js
+++ src/components/ProfileUpdateForm.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const setAxios = require('axios');
+
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.1gC7fqNwCSTYxCQAHvfNmfyb2GhenC6jG0nKLJ-izCM'
+setAxios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
 
 class ProfileUpdateForm extends Component {
   state = {
@@ -10,15 +16,37 @@ class ProfileUpdateForm extends Component {
     this.setState({ name: nextProps.name, bio: nextProps.bio });
   }
 
+  updateProfile = (profileName, bio) => {
+    axios.put(' http://127.0.0.1:8000/api/profile/Kyppy/edit/', { profile: { name: profileName, user_bio: bio } });
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    const { name, bio } = this.state;
+    this.updateProfile(name, bio);
+  }
+
+  handleNameChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  handleBioChange = (e) => {
+    this.setState({
+      bio: e.target.value,
+    });
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleOnSubmit}>
         <div className="form-group">
-          <input type="text" className="form-control" id="editName" defaultValue={this.state.name} />
+          <input type="text" className="form-control" id="editName" defaultValue={this.state.name} onChange={this.handleNameChange} />
           <small id="userNameHelp" className="form-text text-muted"> Edit Your Name</small>
         </div>
         <div className="form-group">
-          <input type="text" className="form-control" id="editBio" defaultValue={this.state.bio} />
+          <input type="text" className="form-control" id="editBio" defaultValue={this.state.bio} onChange={this.handleBioChange} />
           <small id="userNameHelp" className="form-text text-muted"> Edit Your Bio</small>
         </div>
         <button type="submit" className="btn btn-primary">Save</button>
