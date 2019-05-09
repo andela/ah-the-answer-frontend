098e8b55a267241c42b0ff94d4e37adf2e27cf4c
Kyppy Simani <kyppy.simani@andela.com>
2019-05-09 21:56:43 +0300

    Refactored for linting

--- src/actions/profileActions.js
+++ src/actions/profileActions.js
@@ -5,14 +5,17 @@ import * as ProfileAction from '../actiontypes/profiles/profileActionTypes';
 // });
 const setAxios = require('axios');
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyfQ.GUe99Eg95Wbx3b07v-kiUI1njRPNCIiZEt9cA1tf5U0'
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyfQ.GUe99Eg95Wbx3b07v-kiUI1njRPNCIiZEt9cA1tf5U0';
 setAxios.defaults.headers.common = { Authorization: `Bearer ${token}` };
 
 export const updateFollows = () => (dispatch) => {
   axios.get(' http://127.0.0.1:8000/api/follows/count/Kyppy/')
     .then((response) => {
       // handle success
-      dispatch({ type: ProfileAction.UPDATE_FOLLOWS, followCount: response.data.success[0].follows });
+      dispatch({
+        type: ProfileAction.UPDATE_FOLLOWS,
+        followCount: response.data.success[0].follows,
+      });
     })
     .catch((error) => {
       // handle error
@@ -24,7 +27,10 @@ export const updateFollowers = () => (dispatch) => {
   axios.get(' http://127.0.0.1:8000/api/follows/count/Kyppy/')
     .then((response) => {
       // handle success
-      dispatch({ type: ProfileAction.UPDATE_FOLLOWERS, followerCount: response.data.success[1].followers });
+      dispatch({
+        type: ProfileAction.UPDATE_FOLLOWERS,
+        followerCount: response.data.success[1].followers,
+      });
     })
     .catch((error) => {
       // handle error
@@ -36,7 +42,10 @@ export const updateBio = () => (dispatch) => {
   axios.get(' http://127.0.0.1:8000/api/profiles/Kyppy/')
     .then((response) => {
       // handle success
-      dispatch({ type: ProfileAction.UPDATE_BIO, userBio: response.data.profile.user_bio });
+      dispatch({
+        type: ProfileAction.UPDATE_BIO,
+        userBio: response.data.profile.user_bio,
+      });
     })
     .catch((error) => {
       // handle error
@@ -48,7 +57,11 @@ export const updateName = () => (dispatch) => {
   axios.get(' http://127.0.0.1:8000/api/profiles/Kyppy/')
     .then((response) => {
       // handle success
-      dispatch({ type: ProfileAction.UPDATE_NAME, givenName: response.data.profile.name, userName: response.data.profile.username });
+      dispatch({
+        type: ProfileAction.UPDATE_NAME,
+        givenName: response.data.profile.name,
+        userName: response.data.profile.username,
+      });
     })
     .catch((error) => {
       // handle error
@@ -60,7 +73,10 @@ export const fetchAvatar = () => (dispatch) => {
   axios.get(' http://127.0.0.1:8000/api/profiles/Kyppy/')
     .then((response) => {
       // handle success
-      dispatch({ type: ProfileAction.FETCH_AVATAR, profilePicture: response.data.profile.avatar_url });
+      dispatch({
+        type: ProfileAction.FETCH_AVATAR,
+        profilePicture: response.data.profile.avatar_url,
+      });
     })
     .catch((error) => {
       // handle error
