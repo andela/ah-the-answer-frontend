2e91d1ed1912a499d2ce587f80e88c5ae68ecaca
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-05-09 16:38:27 +0300

    [#164829324]Fix merge conflicts

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -12,29 +12,17 @@ export class Login extends Component {
     password: '',
   };
 
-<<<<<<< HEAD
-  handleSubmit = (e) => {
-=======
   handleSubmit = e => {
->>>>>>> 0e51ae870467773d84c25c97461f3ec2cdad36e8
     e.preventDefault();
     this.props.signInUser(this.state);
   };
 
-<<<<<<< HEAD
-  handleChange = (e) => {
-=======
   handleChange = e => {
->>>>>>> 0e51ae870467773d84c25c97461f3ec2cdad36e8
     e.preventDefault();
     this.setState({
       [e.target.type]: e.target.value,
     });
   };
-<<<<<<< HEAD
-
-=======
->>>>>>> 0e51ae870467773d84c25c97461f3ec2cdad36e8
   render() {
     const { authError, errorMessages } = this.props;
     return (
@@ -53,13 +41,8 @@ export class Login extends Component {
             <div className="form-group">
               <div className="social-login">
                 {authError
-<<<<<<< HEAD
-                  ? document.getElementById('emailID').classList.add('is-invalid')
-                    || document.getElementById('passwordID').classList.add('is-invalid')
-=======
                   ? document.getElementById('emailID').classList.add('is-invalid') ||
                     document.getElementById('passwordID').classList.add('is-invalid')
->>>>>>> 0e51ae870467773d84c25c97461f3ec2cdad36e8
                   : null}
 
                 <h5 className="text-center mb-4 mt-5">Sign In with</h5>
@@ -128,15 +111,7 @@ export class Login extends Component {
   }
 }
 
-<<<<<<< HEAD
-export const mapStateToProps = state => ({
-  authError: state.auth.authError,
-  errorMessages: state.auth.errorMessages,
-});
-export const mapDispatchToProps = dispatch => ({
-  signInUser: userData => dispatch(signInUser(userData)),
-});
-=======
+
 export const mapStateToProps = state => {
   return {
     authError: state.auth.authError,
@@ -148,7 +123,6 @@ export const mapDispatchToProps = dispatch => {
     signInUser: userData => dispatch(signInUser(userData)),
   };
 };
->>>>>>> 0e51ae870467773d84c25c97461f3ec2cdad36e8
 
 export default connect(
   mapStateToProps,
