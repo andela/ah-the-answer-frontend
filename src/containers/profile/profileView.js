a3f638b10ca13875b48dc3c1b87e04c00356a933
Kyppy Simani <kyppy.simani@andela.com>
2019-05-09 17:40:44 +0300

    Refactor for readability

--- src/containers/profile/profileView.js
+++ src/containers/profile/profileView.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import profileContainer from './profileContainer';
 import ProfileUpdate from '../../components/ProfileUpdate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class ViewProfile extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="ViewProfile">
-          <Route exact path="" component={profileContainer} />
-          <Route path="/user/editprofile" component={ProfileUpdate} />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const ViewProfile = () => (
+  <BrowserRouter>
+    <div className="ViewProfile">
+      <Route exact path="" component={profileContainer} />
+      <Route path="/user/editprofile" component={ProfileUpdate} />
+    </div>
+  </BrowserRouter>
+);
 export default ViewProfile;
