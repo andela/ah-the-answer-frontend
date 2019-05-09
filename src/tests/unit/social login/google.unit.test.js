30610051514429247ec519326ca1ab028a257694
kirega <mtotomdogo96>
2019-05-09 18:02:12 +0300

    [#164829325]  Update the tests Updates the tests for google [start #164829325] Pivotal Tracker

---
+++ src/tests/unit/social login/google.unit.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { mount, shallow } from '../../enzyme';
+import GoogleLogin from '../../../containers/Login/google';
+import Login from '../../../containers/Login/Login';
+
+
+describe('Google component', () => {
+  it('it renders the button', () => {
+    const wrapper = shallow(<GoogleLogin />);
+    const button = wrapper.find('button');
+    expect(button.exists()).toBe(true);
+    expect(button.type()).toEqual('button');
+  });
+  it('loads in the login page', () => {
+    const wrapper = mount(<Login/>);
+    const login = wrapper.find(GoogleLogin);
+    expect(login).toMatch(GoogleLogin);
+  })
+});
