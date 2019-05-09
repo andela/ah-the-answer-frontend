40bc7217393f6cef75384f09fb02d99b8279b826
Kyppy Simani <kyppy.simani@andela.com>
2019-05-09 14:40:16 +0300

    Add action type to fetch user avatar

--- src/actiontypes/profiles/profileActionTypes.js
+++ src/actiontypes/profiles/profileActionTypes.js
@@ -2,3 +2,4 @@ export const UPDATE_FOLLOWS = 'profile/UPDATE_FOLLOWS';
 export const UPDATE_FOLLOWERS = 'profile/UPDATE_FOLLOWERS';
 export const UPDATE_BIO = 'profile/UPDATE_BIO';
 export const UPDATE_NAME = 'profile/UPDATE_NAME';
+export const FETCH_AVATAR = 'profile/FECTH_AVATAR';
