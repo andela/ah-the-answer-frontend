8ad4d0ebd47dd495bd5b82c7c6270f8d5cd7a120
Kyppy Simani <kyppy.simani@andela.com>
2019-05-09 23:33:42 +0300

    Deleted redundant files

--- src/actiontypes/profiles/profileActionTypes.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_FOLLOWS = 'profile/FETCH_FOLLOWS';
-export const FETCH_FOLLOWERS = 'profile/FETCH_FOLLOWERS';
-export const FETCH_BIO = 'profile/FETCH_BIO';
-export const FETCH_NAME = 'profile/FETCH_NAME';
-export const FETCH_AVATAR = 'profile/FECTH_AVATAR';
