924b404012ad9af69737ab59fa8e1380a8e50287
Kyppy Simani <kyppy.simani@andela.com>
2019-05-09 22:44:34 +0300

    Rename actiontype strings

--- src/actiontypes/profiles/profileActionTypes.js
+++ src/actiontypes/profiles/profileActionTypes.js
@@ -1,5 +1,5 @@
-export const FETCH_FOLLOWS = 'profile/UPDATE_FOLLOWS';
-export const FETCH_FOLLOWERS = 'profile/UPDATE_FOLLOWERS';
-export const FETCH_BIO = 'profile/UPDATE_BIO';
-export const FETCH_NAME = 'profile/UPDATE_NAME';
+export const FETCH_FOLLOWS = 'profile/FETCH_FOLLOWS';
+export const FETCH_FOLLOWERS = 'profile/FETCH_FOLLOWERS';
+export const FETCH_BIO = 'profile/FETCH_BIO';
+export const FETCH_NAME = 'profile/FETCH_NAME';
 export const FETCH_AVATAR = 'profile/FECTH_AVATAR';
