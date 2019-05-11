60351c3d23c8fbcaf267bfac0142813104969818
Kyppy Simani <kyppy.simani@andela.com>
2019-05-11 13:38:35 +0300

    Added default prop

--- src/containers/profile/components/BiographyText.js
+++ src/containers/profile/components/BiographyText.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const BioText = ({ text }) => (
   <div className="container">
-    <p className="text-justify">{text}</p>
+    <p className="text-justify">{text || 'User bio here'}</p>
   </div>
 );
 
