72a32103e065e89707a55f7786d9892a86197911
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-11 15:06:50 +0300

    [#164829327] Adds signed in and signed out links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+import SignedOutLinks from './SignedOutLinks';
+import SignedInLinks from './SignedInLinks';
+import authStatus from '../helpers/authStatus';
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink className="navbar-brand" to="/">Authors Haven</NavLink>
-      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/create">Create Article</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/signin">Sign In</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const links = authStatus() ? <SignedInLinks /> : <SignedOutLinks />;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <NavLink className="navbar-brand" to="/">Authors Haven</NavLink>
+        { links }
+      </div>
+    </nav>
+  );
+};
 
 export default withRouter(Navbar);
---
+++ src/components/SignedInLinks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInLinks = () => (
+  <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+    <li className="nav-item">
+      <NavLink className="nav-link" to="/">Home</NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink className="nav-link" to="/create">Create Article</NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink className="nav-link text-danger" to="/">Sign Out</NavLink>
+    </li>
+  </ul>
+);
+
+export default SignedInLinks;
