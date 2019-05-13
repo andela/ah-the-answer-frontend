38c087ec397867985cc31b7853f8bf6363a2a0a3
Kyppy Simani <kyppy.simani@andela.com>
2019-05-13 16:00:52 +0300

    Added auth reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import profileReducer from './profileReducer';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   profile: profileReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
