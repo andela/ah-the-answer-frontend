4770ca1a59c502d231d6e3a1f72035aa7c6bb7f7
Larry <larry@Larrys-MacBook-Pro.local>
2019-05-13 18:00:28 +0300

    Fix conflicts

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import RequestPasswordReset from './requestPasswordReset';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   resetPassword: RequestPasswordReset,
+  auth:authReducer,
 });
 
 export default rootReducer;
