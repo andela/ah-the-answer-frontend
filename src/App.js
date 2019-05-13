539725d7c1778c6ebce7ec076ae0574b1a5931fe
Kyppy Simani <kyppy.simani@andela.com>
2019-05-13 15:59:08 +0300

    Updated routing and removed redundant files

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import './themes/custom.scss';
 import './themes/bootstrap-custom.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ViewProfile from './containers/profile/profileView';
-
 import Login from './containers/Login/Login';
 import Home from './containers/Home';
-import './themes/custom.scss';
 import Signup from './containers/signup/Signup';
 import SignupSuccess from './containers/signup/SignupSuccess';
+import profileContainer from './containers/profile/profileContainer';
+import ProfileUpdate from './containers/profile/ProfileUpdate';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <ViewProfile />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
           <Route path="/success-signup" component={SignupSuccess} />
+          <Route path="/profile" component={profileContainer} />
+          <Route path="/editprofile" component={ProfileUpdate} />
         </Switch>
       </div>
     </Router>
