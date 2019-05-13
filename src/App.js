b87a18b25424d944f53766a5aca7f675b78e7c05
Kyppy Simani <kyppy.simani@andela.com>
2019-05-13 13:21:06 +0300

    Removed redundant files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './themes/custom.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ViewProfile from './containers/profile/profileView';
 import './themes/bootstrap-custom.scss';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Login from './views/Login'
-import Home from './views/Home'
-import Navbar from './views/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ViewProfile from './containers/profile/profileView';
+
 
 function App() {
   return (
