8a299bc35cd3c86d01dde072b774637fc0c623d2
Larry <larry@Larrys-MacBook-Pro.local>
2019-05-13 18:19:00 +0300

    Fix linting error

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -4,7 +4,7 @@ import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   resetPassword: RequestPasswordReset,
-  auth:authReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
