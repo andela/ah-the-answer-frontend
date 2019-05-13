01c06a63ec6b3cd5202ae506d011092823a2a223
Larry <larry@Larrys-MacBook-Pro.local>
2019-05-13 13:05:01 +0300

    Add tets

--- src/tests/integration/passwordreset/ResetpasswordIntergration.test.js
+++ src/tests/integration/passwordreset/ResetpasswordIntergration.test.js
@@ -2,15 +2,18 @@ import moxios from 'moxios';
 import { createStore , applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../../../store/reducers/rootReducer';
-import { requestResetPasswordSuccess } from '../../../store/actions/requestResetPassword';
+import { requestResetPasswordSuccess, resetPassword} from '../../../store/actions/requestResetPassword';
+import store from '../../../store/store';
 
+
+const mainStore = store;
 
 const testStore = (initialState) => {
   const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
   return createStoreWithMiddleware(rootReducer, initialState);
 };
 
-describe('Test request reset password successful', () => {
+describe('Test request reset password updates store', () => {
   beforeEach(() => {
     moxios.install();
   });
@@ -37,9 +40,19 @@ describe('Test request reset password successful', () => {
       });
     });
     store.dispatch(requestResetPasswordSuccess(resetData));
-    console.log(store.getState())
     const newstate = store.getState().resetPassword;
     // eslint-disable-next-line no-undef
     expect(newstate).toEqual(expectedState);
+  });
+});
+
+describe('Test request reset password success', () => {
+
+
+  // eslint-disable-next-line no-undef
+  it('store is updated successfuly on password reset', () => {
+    const State = mainStore.getState()
+    const InitState = State.resetPassword;
+    expect(InitState).toEqual({ error: '', message: '' });
   });
 });
