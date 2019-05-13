98e64e9925f3230a6a047ecf0870f93a9707b87c
Larry <larry@Larrys-MacBook-Pro.local>
2019-05-13 17:41:32 +0300

    Fix conflicts

--- src/App.js
+++ src/App.js
@@ -8,32 +8,25 @@ import ResetPasswordMessage from './containers/PasswordReset/PasswordResetSucces
 // eslint-disable-next-line import/no-named-as-default
 import ResetPassword from './containers/PasswordReset/resetPassword';
 import './themes/bootstrap-custom.scss';
-<<<<<<< HEAD
 
-=======
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './containers/Login/Login';
 import Home from './containers/Home';
 import './themes/custom.scss';
 import Signup from './containers/signup/Signup';
 import SignupSuccess from './containers/signup/SignupSuccess';
->>>>>>> develop
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-<<<<<<< HEAD
           <Route exact path="/passwordreset" component={InitiatePasswordReset} />
           <Route path="/passwordresetsuccess" component={ResetPasswordMessage} />
           <Route path="/passwordreset/:token" component={ResetPassword} />
-=======
           <Route exact path="/" component={Home} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
           <Route path="/success-signup" component={SignupSuccess} />
->>>>>>> develop
         </Switch>
       </div>
     </Router>
