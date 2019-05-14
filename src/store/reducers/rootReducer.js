62326e820fbc54fba418aaf991fe137e93b0b90d
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-05-15 02:17:17 +0300

    164829329 Feature(Comments CRUD): Users should be able to view article comments, authenticated users should be able to perform CRUD functionality on their comments.
    - Alllow authenticated users to create, update and delete their comments
    - Allow all users to view all comments for all articles
    - Restrict editing or deleting comments to ones that a user has authored
    
    [Finishes #164829329]

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import profileReducer from './profileReducer';
 import RequestPasswordReset from './requestPasswordReset';
 import authReducer from './authReducer';
 import articleReducer from './articleReducer';
+import commentReducer from './commentReducer';
 
 const rootReducer = combineReducers({
   profile: profileReducer,
   resetPassword: RequestPasswordReset,
   auth: authReducer,
   articles: articleReducer,
+  comments: commentReducer,
 });
 
 export default rootReducer;
