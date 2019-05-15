247f5398765519b9b449e21ef357acdbb8451d22
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-05-15 17:22:19 +0300

    [#166030619] Add signout feature and fix 403 status error bug

--- src/components/SignedInLinks.js
+++ src/components/SignedInLinks.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const SignedInLinks = () => (
-  <div data-test="signedInLink">
-    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/">Home</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/create">Create Article</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link text-danger" to="/">Sign Out</NavLink>
-      </li>
-    </ul>
-  </div>
-);
+class SignedInLinks extends Component {
+  handleLogout = () => {
+    localStorage.clear();
+    window.location.replace('/');
+  };
+
+  render() {
+    return (
+      <div data-test="signedInLink">
+        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/create">
+              Create Article
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link text-danger" onClick={this.handleLogout} to="/">
+              Sign Out
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default SignedInLinks;
