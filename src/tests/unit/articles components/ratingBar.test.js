3d34aea66ada4054a1197522485f8959a44bba45
Kyppy Simani <kyppy.simani@andela.com>
2019-05-16 10:24:30 +0300

    Added unit tests for 'star bar'

--- src/tests/unit/articles components/ratingBar.test.js
+++ src/tests/unit/articles components/ratingBar.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount, shallow } from '../../enzyme';
 import RatingBar from '../../../containers/articles/RatingBar';
+import { log } from 'util';
 
 describe('test RatingDisplay component', () => {
     it('should render', () => {
@@ -12,5 +13,40 @@ describe('test RatingDisplay component', () => {
     const wrapper = mount(<RatingBar />);
     expect(wrapper.exists()).toBe(true);
     });
-    
+
+    it('renders default stars when provided no props', () => {
+    const wrapper = mount(
+        <RatingBar />,
+    );
+    const starBar = wrapper.find('span').at(0)
+    expect(starBar.props().style.color).toEqual('#f1f1f1');
+    });
+
+    it('stars render default color "f1f1f1" when provided no props and not clicked', () => {
+    const wrapper = mount(
+        <RatingBar />,
+    );
+    const starBar = wrapper.find('span').at(0)
+    expect(starBar.props().style.color).toEqual('#f1f1f1');
+    });
+
+    it('1st star renders color "ffd700" when clicked', () => {
+    const wrapper = mount(
+        <RatingBar />,
+    );
+    const starBar = wrapper.find('span').at(0)
+    starBar.simulate('click')
+    expect(starBar.instance().style.color).toEqual('rgb(255, 215, 0)');
+    });
+
+    it('change "userRating" state value to 5 when 5th star is clicked', () => {
+    const wrapper = mount(
+        <RatingBar ratingValue={0}/>,
+    );
+    const starBar = wrapper.find('span').at(4)
+    starBar.simulate('click')
+    console.log(starBar.instance().style.color)
+    expect(wrapper.state().userRating).toEqual(5);
+    });
+
 });
