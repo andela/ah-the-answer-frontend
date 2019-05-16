e15bc8a51c447774da18cbee76ce45ffd7e2158f
Kyppy Simani <kyppy.simani@andela.com>
2019-05-16 23:57:52 +0300

    Update component to re-render on rating change

--- src/containers/articles/RatingDisplay.js
+++ src/containers/articles/RatingDisplay.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const RatingDisplay = ({number}) => (
-  <div className="container">
-    <p className="text-center">{number || 0} Star</p>
-  </div>
+export class RatingDisplay extends Component {
+  state ={
+    avgRating: 0,
+  }
+
+  componentDidMount() {
+    this.setState({ avgRating: this.props.rating })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.props.getRating(this.props.slug);
+    this.setState({ avgRating: nextProps.rating });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <p className="text-center">Rated: {this.state.avgRating || 0}</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => (
+  {
+    rating: state.articles.rating,
+    ratingValue: state.articles.ratingValue,
+  }
 );
 
-export default RatingDisplay;
+export default connect(mapStateToProps)(RatingDisplay);
