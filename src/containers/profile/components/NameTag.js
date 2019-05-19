7d9f75122cecf7473ae0c11090fa8b04403feeda
ipaullly <ipaully22@gmail.com>
2019-05-19 18:57:03 +0300

    refactor profile child components

--- src/containers/profile/components/NameTag.js
+++ src/containers/profile/components/NameTag.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import authUser from '../../../helpers/authUser';
+import FollowButton from '../../follow/followButton';
 
+const userDetails = authUser();
 
-const NameTag = ({firstName, secondName}) => (
+const NameTag = ({ firstName, userName, followers }) => (
   <div className="container d-flex align-items-center">
     <div>
       <h3 className="display-4">
         {firstName || 'Name'}
       </h3>
       <h5 className="text-primary">
-        @{secondName || "Username"}
+        @{userName || "Username"}
       </h5>
     </div>
     <div className="ml-auto">
-      <a className="btn btn-outline-primary" href="/editprofile">Edit Profile</a>
+      {
+        (userDetails.username === userName) ? (
+          <a className="btn btn-outline-primary" href="/editprofile">Edit Profile</a>
+        ) : (
+          <FollowButton
+            userName={{ userName }}
+            followers={followers}
+          />
+        )
+      }
     </div>
   </div>
 );
--- src/containers/profile/components/SocialFollowing.js
+++ src/containers/profile/components/SocialFollowing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SocialFollowing = ({socialName, number}) => (
   <div className="container">
-    <p className="text-center">{number || 999} {socialName || "Social Media Point"}</p>
+    <p className="text-center">{number || 0} {socialName}</p>
   </div>
 );
 
