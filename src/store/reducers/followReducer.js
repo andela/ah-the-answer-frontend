50afb2fae139780f122b0c5348b2680e7e8b2188
ipaullly <ipaully22@gmail.com>
2019-05-19 19:08:01 +0300

    Add reducer to handle follow/unfollow actions

--- src/store/reducers/followReducer.js
+++ src/store/reducers/followReducer.js
@@ -1,8 +1,9 @@
 const initialState = {
-  following: [],
+  followings: [],
   followers: [],
   follow: false,
   unfollow: false,
+  checkfollow: false,
   error: '',
 };
 const followingReducer = (state = initialState, action) => {
@@ -14,6 +15,18 @@ const followingReducer = (state = initialState, action) => {
     case 'UNFOLLOW_USER':
       return { ...state, unfollow: action.payload };
     case 'UNFOLLOW_USER_ERROR':
+      return { ...state, error: action.error };
+    case 'GET_FOLLOWERS_SUCCESS':
+      return { ...state, followers: action.payload };
+    case 'GET_FOLLOWERS_ERROR':
+      return { ...state, error: action.error };
+    case 'GET_FOLLOWINGS_SUCCESS':
+      return { ...state, followings: action.payload };
+    case 'GET_FOLLOWINGS_ERROR':
+      return { ...state, error: action.error };
+    case 'CHECK_FOLLOW_USER':
+      return { ...state, checkfollow: action.payload };
+    case 'CHECK_FOLLOW_USER_ERROR':
       return { ...state, error: action.error };
     default:
       return state;
