a50d60bf74e55242bcef4b5d355f5870b2fc239f
ipaullly <ipaully22@gmail.com>
2019-05-19 19:02:49 +0300

    Make API calls in the follow action creators

--- src/store/actions/followActions.js
+++ src/store/actions/followActions.js
@@ -11,18 +11,39 @@ const config = {
 
 export const follow = (userName) => {
   return (dispatch) => {
-    console.log(userName);
     return axios.post(
-      `${configUrls.root}/follow/${userName}`,
+      `${configUrls.root}/follow/${userName}/`,
+      {},
       config,
     )
-      .then(() => {
-        dispatch({ type: 'FOLLOW_USER', payload: true });
+      .then((response) => {
+        if (response) {
+          dispatch({ type: 'FOLLOW_USER', payload: true });
+        }
       })
       .catch((error) => {
         if (error) {
-          console.log(error);
           dispatch({ type: 'FOLLOW_USER_ERROR', error });
+        }
+      });
+  };
+};
+
+export const checkFollow = (userName) => {
+  return (dispatch) => {
+    return axios.post(
+      `${configUrls.root}/checkfollow/${userName}/`,
+      {},
+      config,
+    )
+      .then((response) => {
+        if (response) {
+          dispatch({ type: 'CHECK_FOLLOW_USER', payload: response.data.success });
+        }
+      })
+      .catch((error) => {
+        if (error) {
+          dispatch({ type: 'CHECK_FOLLOW_USER_ERROR', error });
         }
       });
   };
@@ -32,14 +53,59 @@ export const unFollow = (userName) => {
   return (dispatch) => {
     return axios.delete(
       `${configUrls.root}/unfollow/${userName}`,
-      {},
       config,
     )
-      .then(() => {
-        dispatch({ type: 'UNFOLLOW_USER', payload: true });
+      .then((response) => {
+        if (response) {
+          dispatch({ type: 'UNFOLLOW_USER', payload: true });
+        }
       })
       .catch((error) => {
         dispatch({ type: 'UNFOLLOW_USER_ERROR', error: error.response.data });
+      });
+  };
+};
+
+export const getFollowers = () => {
+  return (dispatch) => {
+    return axios.get(
+      `${configUrls.root}/followers/`,
+      config,
+    )
+      .then((response) => {
+        if (response) {
+          dispatch({
+            type: 'GET_FOLLOWERS_SUCCESS',
+            payload: response.data.followers,
+          });
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          dispatch({ type: 'GET_FOLLOWERS_ERROR', error: error.response.data });
+        }
+      });
+  };
+};
+
+export const getFollowings = () => {
+  return (dispatch) => {
+    return axios.get(
+      `${configUrls.root}/followings/`,
+      config,
+    )
+      .then((response) => {
+        if (response) {
+          dispatch({
+            type: 'GET_FOLLOWINGS_SUCCESS',
+            payload: response.data.followed_users,
+          });
+        }
+      })
+      .catch((error) => {
+        if (error) {
+          dispatch({ type: 'GET_FOLLOWINGS_ERROR', error: error.data });
+        }
       });
   };
 };
