7d18deaa4e3bb8789171c26bfe6c37e771e31a6e
Larry <larry@Larrys-MacBook-Pro.local>
2019-05-21 22:31:27 +0300

    [Feature 164829336] User can opt in and out of notifications

--- src/containers/notifications/NotificationList.js
+++ src/containers/notifications/NotificationList.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import authHeader from '../../helpers/authHeader';
 import NotificationSummary from './NotificationSummary';
-
+import Toggle from './ToggleSwitch';
 
 const url = 'https://ah-the-answer-backend-staging.herokuapp.com/api/notifications/';
+const Readurl = 'https://ah-the-answer-backend-staging.herokuapp.com/api/notifications/read';
 const readUrl = 'https://ah-the-answer-backend-staging.herokuapp.com/api/notifications/read-all';
+const statusUrl = 'https://ah-the-answer-backend-staging.herokuapp.com/api/notifications/subscription';
 const config = {
   headers: authHeader(),
 
@@ -14,16 +16,41 @@ const config = {
 class NotificationList extends Component {
     state = {
       notifications: [],
+      isSubscribed: null,
     }
 
     componentDidMount() {
-      const response1 = axios.get(url, config)
+      this.getNotifications();
+    }
+
+    getNotifications = () => {
+      axios.get(statusUrl, config)
         .then((res) => {
-          const { notifications } = res.data;
-          this.setState({ notifications });
+          const { status } = res.data.subscription;
+          this.setState({ isSubscribed: status });
+          const { isSubscribed } = this.state;
+          if (isSubscribed === true) {
+            axios.get(url, config)
+              .then((r) => {
+                const { notifications } = r.data;
+                this.setState({ notifications });
+              });
+            axios.put(readUrl, {}, config);
+          } else {
+            axios.get(Readurl, config)
+              .then((re) => {
+                const { notifications } = re.data;
+                this.setState({ notifications });
+              });
+          }
         });
-      axios.put(readUrl, {}, config);
-      return response1;
+    }
+
+    handleClick = () => {
+      this.setState(prevState => ({
+        isSubscribed: !prevState.isSubscribed,
+      }));
+      axios.post(statusUrl, {}, config);
     }
 
     render() {
@@ -31,6 +58,8 @@ class NotificationList extends Component {
       if (notifications < 1) {
         return (
           <div className="container" data-test="articleListNone">
+
+            <Toggle handleClick={this.handleClick} isSubscribed={this.state.isSubscribed} />
             <div className="d-flex justify-content-center">
               <p className="text-muted mt-1 ml-1">You have no notifications</p>
             </div>
@@ -39,6 +68,7 @@ class NotificationList extends Component {
       }
       return (
         <div className="list-group container p-4">
+          <Toggle handleClick={this.handleClick} isSubscribed={this.state.isSubscribed} />
           { notifications.map(notification => (
             <NotificationSummary notification={notification} />
           )) }
