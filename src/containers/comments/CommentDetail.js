8c6425ef6895ba16c18ab2abc7d14a634f6b5342
Maina Kamau <mainakamau@Mainas-MacBook-Pro.local>
2019-05-22 10:03:14 +0300

    164829344 Feature(Display Comment History): Users should be able to see the comment history of an edited comment
    - Write tests for CommentHistory functionality
    - View comment history of edited comments
    - Make comment edit history accessible to all users
    
    [Finishes #164829344]

--- src/containers/comments/CommentDetail.js
+++ src/containers/comments/CommentDetail.js
@@ -1,32 +1,46 @@
+/* eslint-disable no-shadow */
 /* eslint-disable no-nested-ternary */
 import React from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import CommentHistory from '../commentHistory/CommentHistory';
 import { deleteComment, editComment } from '../../store/actions/commentActions';
 
 export class CommentDetail extends React.Component {
   state = {
     body: '',
     isHidden: true,
+    historyReveal: false,
+    revealEdits: false,
   };
 
   componentDidMount() {
+    const { item } = this.props;
     this.setState({
-      body: this.props.item.body,
+      body: item.body,
     });
   }
 
   handleDelete = (id) => {
-    this.props.deleteComment(this.props.slug, id);
+    const { slug, deleteComment } = this.props;
+    deleteComment(slug, id);
   };
 
   handleSubmit = (id) => {
-    this.props.editComment(this.props.slug, id, this.state);
+    const { slug, editComment } = this.props;
+    editComment(slug, id, this.state);
   };
 
-  handleOpen = (e) => {
+  handleOpen = () => {
     this.setState({ isHidden: false });
   };
+
+  handleReveal = () => (!this.state.historyReveal
+    ? this.setState({ historyReveal: true })
+    : this.setState({ historyReveal: false }));
 
   handleChange = (e) => {
     e.preventDefault();
@@ -37,42 +51,77 @@ export class CommentDetail extends React.Component {
 
   render() {
     const userObject = JSON.parse(localStorage.getItem('user'));
+    const { item, slug } = this.props;
+    const { isHidden, historyReveal, revealEdits } = this.state;
     return (
       <div className="card mb-3 mt-3 mx-auto">
         <div className="card-body">
           <p className="card-text">
-            <div>
-              {this.state.isHidden ? (
+            <div
+              onMouseOver={() => this.setState({ revealEdits: true })}
+              onMouseLeave={() => this.setState({ revealEdits: false })}
+            >
+              {isHidden ? (
                 <div>
-                  <p className="card-text">{this.props.item.body}</p>
-                  <div className="d-flex">
-                    <small className="text-muted ml-auto">
-                      {moment(this.props.item.createdAt).fromNow()}
-                    </small>
+                  <div className="d-flex mb-3">
+                  <FontAwesomeIcon icon="user" color="#20c997" className="comment-icon mt-1" size="3x"/>
+                    <div className="d-flex flex-column bd-highlight mt-2 ml-3">
+                      <a href={`/profile/${item.author.username}`}>
+                        <small className="bd-highlight">{item.author.username}</small>
+                      </a>
+                      <small className="bd-highlight text-muted">{item.author.email}</small>
+                    </div>
                   </div>
-                  {userObject && userObject.username !== this.props.item.author.username ? (
+                  <p className="card-text mb-1">{item.body}</p>
+                  {historyReveal ? (
+                    <CommentHistory slug={slug} id={item.id} user={item.author} />
+                  ) : null}
+                  <div className="d-flex">
+                    {moment(item.createdAt).format() !== moment(item.updatedAt).format() ? (
+                      <span onClick={this.handleReveal} className="btnEdit info">
+                        {!historyReveal ? 'Edited' : 'Close'}
+                      </span>
+                    ) : null}
+                    <small className="text-muted ml-auto">{moment(item.createdAt).fromNow()}</small>
+                  </div>
+                  {userObject && userObject.username !== item.author.username ? (
                     <div />
                   ) : !userObject ? (
                     <div />
+                  ) : revealEdits ? (
+                    <div className="d-flex w-100 card-text">
+                      <div className="w-25 m-auto d-flex mt-2">
+                        <FontAwesomeIcon icon="edit" className="col editIcon" onClick={this.handleOpen}>
+                          Edit
+                        </FontAwesomeIcon>
+
+                        <FontAwesomeIcon
+                          className="col trashIcon"
+                          icon="trash"
+                          onClick={() => {
+                            if (window.confirm('Are you sure you wish to delete this item?')) this.handleDelete(item.id);
+                          }}
+                        />
+                      </div>
+                    </div>
                   ) : (
                     <div className="d-flex w-100 card-text">
-                      <div className="w-25 mr-auto d-flex align-items-center">
-                        <button
-                          type="button"
-                          className="btn btn-primary btn-sm col mr-2"
+                      <div className="w-25 m-auto d-flex mt-2">
+                        <FontAwesomeIcon
+                          icon="edit"
+                          className="col text-white"
                           onClick={this.handleOpen}
                         >
                           Edit
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-danger col btn-sm"
+                        </FontAwesomeIcon>
+
+                        <FontAwesomeIcon
+                          className="col text-white"
+                          icon="trash"
                           onClick={() => {
-                            if (window.confirm('Are you sure you wish to delete this item?')) this.handleDelete(this.props.item.id);
+                            if (window.confirm('Are you sure you wish to delete this item?')) this.handleDelete(item.id);
                           }}
-                        >
-                          Delete
-                        </button>
+                        />
                       </div>
                     </div>
                   )}
@@ -85,14 +134,14 @@ export class CommentDetail extends React.Component {
                     rows="5"
                     onChange={this.handleChange}
                     className="form-control edit-comment"
-                    defaultValue={this.props.item.body}
+                    defaultValue={item.body}
                   />
 
                   <div className="text-center mt-2">
                     <button
                       type="button"
                       className="btn btn-primary btn-sm col-2"
-                      onClick={() => this.handleSubmit(this.props.item.id)}
+                      onClick={() => this.handleSubmit(item.id)}
                     >
                       Update
                     </button>
@@ -106,6 +155,12 @@ export class CommentDetail extends React.Component {
     );
   }
 }
+CommentDetail.propTypes = {
+  item: PropTypes.shape({}).isRequired,
+  slug: PropTypes.string.isRequired,
+  deleteComment: PropTypes.func.isRequired,
+  editComment: PropTypes.func.isRequired,
+};
 
 export const mapStateToProps = state => ({
   comments: state.comments,
