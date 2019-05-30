17974e4dc3de600742dd89ddde50b23195ad6c75
kirega <mtotomdogo96>
2019-05-30 21:20:11 +0300

    [finishes 164829343] Tests the highlighting feature.

--- src/tests/unit/articles/document.actions.js
+++ src/tests/unit/articles/document.actions.js
@@ -4,12 +4,5 @@ document = {
       display: jest.fn()
     }
   },
-  onmouseup : jest.fn(),
-  getSelection: jest.fn().mockImplementation(() => {
-    return {
-      baseOffset: 1,
-      extentOffset: 10,
-      toString: jest.fn().mockReturnValue('there is some text here')
-    }
-  })
+  onmouseup : jest.fn()
 };
